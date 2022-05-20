1835b4a12d055324e10f07f1993e022e63767b31
Bat-Baat <97858296+Bat-Baat@users.noreply.github.com>
2022-05-20 19:34:38 -0400

    Add files via upload

---
+++ AMazeByBatB/src/edu/wm/cs/cs301/BatB/falstad/RangeSetElement.java
@@ -0,0 +1,24 @@
+package edu.wm.cs.cs301.BatB.falstad;
+
+import java.io.Serializable;
+
+/**
+ * 
+ * This code is refactored code from Maze.java by Paul Falstad, www.falstad.com, Copyright (C) 1998, all rights reserved
+ * Paul Falstad granted permission to modify and use code for teaching purposes.
+ *
+ */
+public class RangeSetElement implements Serializable{
+	public int min, max;
+
+	/**
+	 * Constructor
+	 * @param mn
+	 * @param mx
+	 */
+	RangeSetElement(int mn, int mx) {
+		min = mn;
+		max = mx;
+	}
+}
+
